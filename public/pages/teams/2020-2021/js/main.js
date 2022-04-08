5f53aa53b062435df5cf5386634751df218c4a29
Suriya KS <76834126+Suriya-2002@users.noreply.github.com>
2022-04-08 08:45:53 +0530

    Added particles backgroud in teams page

--- public/pages/teams/2020-2021/js/main.js
+++ public/pages/teams/2020-2021/js/main.js
@@ -280,5 +280,50 @@ const searchMembers = () => {
     });
 };
 
+particlesJS('particles-js', {
+    particles: {
+        number: { value: 150, density: { enable: true, value_area: 800 } },
+        color: { value: '#000' },
+        shape: {
+            type: 'circle',
+            stroke: { width: 0, color: '#000' },
+            polygon: { nb_sides: 5 },
+        },
+        opacity: {
+            value: 0.4,
+            random: false,
+            anim: { enable: true, speed: 0.3, opacity_min: 0.1, sync: false },
+        },
+        size: { value: 3, random: true, anim: { enable: true, speed: 10, size_min: 0.1, sync: false } },
+        line_linked: { enable: true, distance: 150, color: '#000', opacity: 0.2, width: 1 },
+        move: {
+            enable: true,
+            speed: 3,
+            direction: 'none',
+            random: false,
+            straight: false,
+            out_mode: 'out',
+            bounce: false,
+            attract: { enable: false, rotateX: 600, rotateY: 1200 },
+        },
+    },
+    interactivity: {
+        detect_on: 'canvas',
+        events: {
+            onhover: { enable: true, mode: 'repulse' },
+            onclick: { enable: true, mode: 'push' },
+            resize: true,
+        },
+        modes: {
+            grab: { distance: 400, line_linked: { opacity: 1 } },
+            bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
+            repulse: { distance: 200, duration: 0.4 },
+            push: { particles_nb: 4 },
+            remove: { particles_nb: 2 },
+        },
+    },
+    retina_detect: true,
+});
+
 renderQuotes();
 searchMembers();
